1f8b3f8d9dc3d63e6c91d055d19a4a8524b55760
Takayuki Maruyama <maruTA-bis5@users.noreply.github.com>
2019-01-05 21:10:55 +0900

    Add License management APIs (#119)
    * Add Upload License File API
    
    fix #28
    
    * Add Remove License API
    
    fix #29

--- mattermost4j-core/src/main/java/net/bis5/mattermost/client4/MattermostClient.java
+++ mattermost4j-core/src/main/java/net/bis5/mattermost/client4/MattermostClient.java
@@ -1659,6 +1659,22 @@ public class MattermostClient
     return doApiGet("/analytics/old" + queryBuilder.toString(), null, AnalyticsRows.class);
   }
 
+  @Override
+  public ApiResponse<Boolean> uploadLicenseFile(Path licenseFile) {
+    FormDataMultiPart multiPart = new FormDataMultiPart();
+    multiPart.setMediaType(MediaType.MULTIPART_FORM_DATA_TYPE);
+
+    FileDataBodyPart body = new FileDataBodyPart("license", licenseFile.toFile());
+    multiPart.bodyPart(body);
+
+    return doApiPostMultiPart("/license", multiPart).checkStatusOk();
+  }
+
+  @Override
+  public ApiResponse<Boolean> removeLicense() {
+    return doApiDelete("/license").checkStatusOk();
+  }
+
   // Webhooks Section
 
   /**
