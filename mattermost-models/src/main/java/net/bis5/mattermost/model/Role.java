cd591ec3541b52d736d8f8b2cc0f5de5f47bb9b0
Takayuki Maruyama <bis5.wsys@gmail.com>
2019-01-20 07:05:21 +0900

    Update definitions of roles and permissions

--- mattermost-models/src/main/java/net/bis5/mattermost/model/Role.java
+++ mattermost-models/src/main/java/net/bis5/mattermost/model/Role.java
@@ -14,66 +14,24 @@
 
 package net.bis5.mattermost.model;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import lombok.Getter;
 
 /**
- * The type of roles.
+ * Built-in role definitions.
  * 
  * @author Takayuki Maruyama
  */
 @Getter
 public enum Role {
 
-  ROLE_CHANNEL_USER("channel_user", "authentication.roles.channel_user",
-      Arrays.asList(Permission.READ_CHANNEL, Permission.MANAGE_PUBLIC_CHANNEL_MEMBERS,
-          Permission.UPLOAD_FILE, Permission.GET_PUBLIC_LINK, Permission.CREATE_POST,
-          Permission.EDIT_POST, Permission.USE_SLASH_COMMANDS)), //
-  ROLE_CHANNEL_ADMIN("channel_admin", "authentication.roles.channel_admin",
-      Arrays.asList(Permission.MANAGE_CHANNEL_ROLES)), //
-  ROLE_CHANNEL_GUEST("guest", "authentication.roles.global_guest", Collections.emptyList()), //
-  ROLE_TEAM_USER("team_user", "authentication.roles.team_user",
-      Arrays.asList(Permission.LIST_TEAM_CHANNELS, Permission.JOIN_PUBLIC_CHANNELS,
-          Permission.READ_PUBLIC_CHANNEL, Permission.VIEW_TEAM)), //
-  ROLE_TEAM_ADMIN("team_admin", "authentication.roles.team_admin",
-      Arrays.asList(Permission.EDIT_OTHERS_POSTS, Permission.REMOVE_USER_FROM_TEAM,
-          Permission.MANAGE_TEAM, Permission.IMPORT_TEAM, Permission.MANAGE_TEAM_ROLES,
-          Permission.MANAGE_CHANNEL_ROLES, Permission.MANAGE_OTHER_WEBHOOKS,
-          Permission.MANAGE_SLASH_COMMANDS, Permission.MANAGE_OTHERS_SLASH_COMMANDS,
-          Permission.MANAGE_WEBHOOKS)), //
-  ROLE_SYSTEM_USER("system_user", "authentication.roles.global_user",
-      Arrays.asList(Permission.CREATE_DIRECT_CHANNEL, Permission.CREATE_GROUP_CHANNEL,
-          Permission.PERMANENT_DELETE_USER)), //
-  ROLE_SYSTEM_ADMIN("system_admin", "authentication.roles.global_admin",
-      Arrays.asList(Permission.values()));
+  SYSTEM_USER, SYSTEM_ADMIN, SYSTEM_POST_ALL, SYSTEM_POST_ALL_PUBLIC, SYSTEM_USER_ACCESS_TOKEN, //
+  TEAM_USER, TEAM_ADMIN, TEAM_POST_ALL, TEAM_POST_ALL_PUBLIC, //
+  CHANNEL_USER, CHANNEL_ADMIN;
 
-  private String id;
-  private String name;
-  private String description;
-  private List<Permission> permissions;
+  private final String roleName;
 
-  private Role(String id, String key, List<Permission> permissions) {
-    this.id = id;
-    this.name = key + ".name";
-    this.description = key + ".description";
-    this.permissions = permissions;
-  }
-
-  /**
-   * Get a role constant for provided {@code roles}.
-   */
-  public boolean is(String roles) {
-    if (roles == null) {
-      return false;
-    }
-    for (String role : roles.split(" ")) {
-      if (this.getId().equals(role)) {
-        return true;
-      }
-    }
-    return false;
+  Role() {
+    this.roleName = name().toLowerCase();
   }
 
 }
