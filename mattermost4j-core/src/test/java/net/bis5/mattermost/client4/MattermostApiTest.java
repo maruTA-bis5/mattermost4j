46e5a9466282e2d82f8809ca06c8799b2c203fa6
Takayuki Maruyama <bis5.wsys@gmail.com>
2019-07-03 07:50:18 +0900

    Add Bots API

--- mattermost4j-core/src/test/java/net/bis5/mattermost/client4/MattermostApiTest.java
+++ mattermost4j-core/src/test/java/net/bis5/mattermost/client4/MattermostApiTest.java
@@ -77,6 +77,9 @@ import net.bis5.mattermost.model.AnalyticsRows;
 import net.bis5.mattermost.model.Audit;
 import net.bis5.mattermost.model.Audits;
 import net.bis5.mattermost.model.AuthService;
+import net.bis5.mattermost.model.Bot;
+import net.bis5.mattermost.model.BotPatch;
+import net.bis5.mattermost.model.Bots;
 import net.bis5.mattermost.model.Channel;
 import net.bis5.mattermost.model.ChannelList;
 import net.bis5.mattermost.model.ChannelMember;
@@ -3865,5 +3868,185 @@ public class MattermostApiTest {
     }
   }
 
+  @Nested
+  class BotsApiTest {
+    @Test
+    public void createBot() {
+      th.logout().loginSystemAdmin();
+      ApiResponse<Config> configResponse = client.getConfig();
+      if (isNotSupportVersion("5.10.0", configResponse)) {
+        return;
+      }
+      Config config = configResponse.readEntity();
+      config.getServiceSettings().setEnableBotAccountCreation(true);
+      assertNoError(client.updateConfig(config));
 
+      BotPatch bot = new BotPatch();
+      bot.setUsername("bot_" + th.newId());
+      bot.setDisplayName("botsApiTestName");
+      bot.setDescription("Bot account for @" + bot.getUsername());
+
+      ApiResponse<Bot> response = assertNoError(client.createBot(bot));
+
+      Bot createdBot = response.readEntity();
+      assertEquals(th.systemAdminUser().getId(), createdBot.getOwnerId());
+      assertEquals(bot.getUsername(), createdBot.getUsername());
+      assertEquals(bot.getDisplayName(), createdBot.getDisplayName());
+      assertEquals(bot.getDescription(), createdBot.getDescription());
+    }
+
+    @Test
+    public void patchBot() {
+      th.logout().loginSystemAdmin();
+      ApiResponse<Config> configResponse = client.getConfig();
+      if (isNotSupportVersion("5.10.0", configResponse)) {
+        return;
+      }
+      Config config = configResponse.readEntity();
+      config.getServiceSettings().setEnableBotAccountCreation(true);
+      assertNoError(client.updateConfig(config));
+
+      BotPatch patch = new BotPatch();
+      patch.setUsername("bot_" + th.newId());
+      patch.setDisplayName("botsApiTestName");
+      patch.setDescription("Bot account for @" + patch.getUsername());
+      Bot originalBot = assertNoError(client.createBot(patch)).readEntity();
+
+      patch.setUsername("up_" + th.newId());
+      patch.setDisplayName("up_" + patch.getDisplayName());
+      patch.setDescription("up_" + patch.getDescription());
+
+      ApiResponse<Bot> response = assertNoError(client.patchBot(originalBot.getUserId(), patch));
+
+      Bot patchedBot = response.readEntity();
+      assertEquals(patch.getUsername(), patchedBot.getUsername());
+      assertEquals(patch.getDisplayName(), patchedBot.getDisplayName());
+      assertEquals(patch.getDescription(), patchedBot.getDescription());
+    }
+
+    @Test
+    public void getBot() {
+      th.logout().loginSystemAdmin();
+      ApiResponse<Config> configResponse = client.getConfig();
+      if (isNotSupportVersion("5.10.0", configResponse)) {
+        return;
+      }
+      Config config = configResponse.readEntity();
+      config.getServiceSettings().setEnableBotAccountCreation(true);
+      assertNoError(client.updateConfig(config));
+
+      BotPatch patch = new BotPatch();
+      patch.setUsername("bot_" + th.newId());
+      patch.setDisplayName("botsApiTestName");
+      patch.setDescription("Bot account for @" + patch.getUsername());
+      Bot originalBot = assertNoError(client.createBot(patch)).readEntity();
+
+      ApiResponse<Bot> response = assertNoError(client.getBot(originalBot.getUserId()));
+
+      Bot receiveddBot = response.readEntity();
+      assertEquals(originalBot.getUsername(), receiveddBot.getUsername());
+      assertEquals(originalBot.getDisplayName(), receiveddBot.getDisplayName());
+      assertEquals(originalBot.getDescription(), receiveddBot.getDescription());
+    }
+
+    @Test
+    public void getBots() {
+      th.logout().loginSystemAdmin();
+      ApiResponse<Config> configResponse = client.getConfig();
+      if (isNotSupportVersion("5.10.0", configResponse)) {
+        return;
+      }
+      Config config = configResponse.readEntity();
+      config.getServiceSettings().setEnableBotAccountCreation(true);
+      assertNoError(client.updateConfig(config));
+
+      BotPatch patch = new BotPatch();
+      patch.setUsername("bot1_" + th.newId());
+      patch.setDisplayName("botsApiTestName1");
+      patch.setDescription("Bot account for @" + patch.getUsername());
+      Bot originalBot1 = assertNoError(client.createBot(patch)).readEntity();
+
+      patch = new BotPatch();
+      patch.setUsername("bot2_" + th.newId());
+      patch.setDisplayName("botsApiTestName2");
+      patch.setDescription("Bot account for @" + patch.getUsername());
+      Bot originalBot2 = assertNoError(client.createBot(patch)).readEntity();
+
+      Bots bots = assertNoError(client.getBots()).readEntity();
+      Set<String> botUserIds = bots.stream() //
+          .map(Bot::getUserId) //
+          .collect(Collectors.toSet());
+      assertThat(botUserIds, hasItems(originalBot1.getUserId(), originalBot2.getUserId()));
+    }
+
+    @Test
+    public void disableBot() {
+      th.logout().loginSystemAdmin();
+      ApiResponse<Config> configResponse = client.getConfig();
+      if (isNotSupportVersion("5.10.0", configResponse)) {
+        return;
+      }
+      Config config = configResponse.readEntity();
+      config.getServiceSettings().setEnableBotAccountCreation(true);
+      assertNoError(client.updateConfig(config));
+
+      BotPatch patch = new BotPatch();
+      patch.setUsername("bot_" + th.newId());
+      patch.setDisplayName("botsApiTestName");
+      patch.setDescription("Bot account for @" + patch.getUsername());
+      Bot originalBot = assertNoError(client.createBot(patch)).readEntity();
+
+      ApiResponse<Bot> response = assertNoError(client.disableBot(originalBot.getUserId()));
+      assertTrue(response.readEntity().getDeleteAt() != 0L);
+    }
+
+    @Test
+    public void enableBot() {
+      th.logout().loginSystemAdmin();
+      ApiResponse<Config> configResponse = client.getConfig();
+      if (isNotSupportVersion("5.10.0", configResponse)) {
+        return;
+      }
+      Config config = configResponse.readEntity();
+      config.getServiceSettings().setEnableBotAccountCreation(true);
+      assertNoError(client.updateConfig(config));
+
+      BotPatch patch = new BotPatch();
+      patch.setUsername("bot_" + th.newId());
+      patch.setDisplayName("botsApiTestName");
+      patch.setDescription("Bot account for @" + patch.getUsername());
+      Bot originalBot = assertNoError(client.createBot(patch)).readEntity();
+      ApiResponse<Bot> response = assertNoError(client.disableBot(originalBot.getUserId()));
+      assertTrue(response.readEntity().getDeleteAt() != 0L);
+
+
+      response = assertNoError(client.enableBot(originalBot.getUserId()));
+      assertTrue(response.readEntity().getDeleteAt() == 0L);
+    }
+
+    @Test
+    public void assignBotToUser() {
+      th.logout().loginSystemAdmin();
+      ApiResponse<Config> configResponse = client.getConfig();
+      if (isNotSupportVersion("5.10.0", configResponse)) {
+        return;
+      }
+      Config config = configResponse.readEntity();
+      config.getServiceSettings().setEnableBotAccountCreation(true);
+      assertNoError(client.updateConfig(config));
+
+      BotPatch patch = new BotPatch();
+      patch.setUsername("bot_" + th.newId());
+      patch.setDisplayName("botsApiTestName");
+      patch.setDescription("Bot account for @" + patch.getUsername());
+      Bot originalBot = assertNoError(client.createBot(patch)).readEntity();
+      assertEquals(th.systemAdminUser().getId(), originalBot.getOwnerId());
+
+      User secondSysAdmin = th.createSystemAdminUser();
+
+      ApiResponse<Bot> response =
+          assertNoError(client.assignBotToUser(originalBot.getUserId(), secondSysAdmin.getId()));
+      assertEquals(secondSysAdmin.getId(), response.readEntity().getOwnerId());
+    }
+  }
 }
